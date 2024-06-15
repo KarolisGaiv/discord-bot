bddb24784416f7a13f0c4d12f99f0fb615bc394a
KarolisG <kgaivenis@gmail.com>
2024-06-15 16:13:52 +0300

    chore: add script to run discord bot commands

--- src/bot/commands/utils.ts
+++ src/bot/commands/utils.ts
@@ -5,5 +5,5 @@ export const data = new SlashCommandBuilder()
     .setDescription('Replies with Pong!');
 
 export async function execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply('https://media0.giphy.com/media/DYH297XiCS2Ck/giphy.gif?cid=bcd3895fobiy7y5basnecggtsvxdc5i81y3fswgczjzmfo7q&ep=v1_gifs_random&rid=giphy.gif&ct=g');
 }
