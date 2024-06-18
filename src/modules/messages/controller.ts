47d06e2b19dfd889fbababcb8d2fa16ff3dbdbc6
KarolisG <kgaivenis@gmail.com>
2024-06-18 13:53:21 +0300

    chore: format code

--- src/modules/messages/controller.ts
+++ src/modules/messages/controller.ts
@@ -9,7 +9,6 @@ import { getRandomTemplate } from '../message-templates/services';
 import { findSprintByCode } from '../sprints/services';
 import { sendMessageToDiscord } from '@/bot/utils/sendMessage';
 
-
 export function createMessagesRouter(db: Kysely<DB>) {
   const router = Router();
 
@@ -20,20 +19,22 @@ export function createMessagesRouter(db: Kysely<DB>) {
       username: req.query.userName || req.query.username,
       sprintCode: req.query.code || req.query.sprintCode,
     };
-  
+
     const parsedInput = schema.parsePartialInput(query);
     const { username, sprintCode } = parsedInput;
-  
+
     try {
       if (username) {
         // Fetch messages for the specific user from the database
-        const messagesList = await messages.findMessagesByUserName(db, 
+        const messagesList = await messages.findMessagesByUserName(
+          db,
           username as string
         );
         res.status(200).json(messagesList);
       } else if (sprintCode) {
         // Fetch messages for the specific sprint from the database
-        const messagesList = await messages.findMessagesBySprintCode(db, 
+        const messagesList = await messages.findMessagesBySprintCode(
+          db,
           sprintCode as string
         );
         res.status(200).json(messagesList);
@@ -50,25 +51,25 @@ export function createMessagesRouter(db: Kysely<DB>) {
       }
     }
   });
-  
+
   // Send success message to Discord
   router.post('/', async (req, res) => {
     try {
       const gifUrl = await getGIF();
       const message = await getRandomTemplate(db);
       const { title } = await findSprintByCode(db, req.body.sprintCode);
-  
+
       // Combine the message body with the fetched gifUrl
       const newMessageData = {
         ...req.body,
         message,
         gifUrl,
       };
-  
+
       schema.parseFullSchema(newMessageData);
-  
+
       const newMessage = await messages.create(db, newMessageData);
-  
+
       await sendMessageToDiscord(newMessageData, title);
       res.status(200).json(newMessage);
     } catch (err) {
@@ -76,8 +77,5 @@ export function createMessagesRouter(db: Kysely<DB>) {
     }
   });
 
-  return router
+  return router;
 }
-
-
-
